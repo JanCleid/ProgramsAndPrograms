021e1e2233da0edd577e290ebf3a896b1c9c85eb
JanCleid <43229619+JanCleid@users.noreply.github.com>
2019-02-03 01:52:40 +0800

    Add files via upload
    While Loop Using PHP

---
+++ whileloop.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>While Loop</title>
+</head>
+<body>
+
+	<?php
+	//While loop, repeat the statement
+	$x = 1;
+	while ($x < 5) {
+		echo "While Loop<br>";
+		$x++;
+	}
+
+	?>
+</body>
+</html>
